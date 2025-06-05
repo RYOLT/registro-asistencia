cf9801ac924bf18206343c8934587b1c5bed1e2a
Rodrigo <143373606+RYOLT@users.noreply.github.com>
2025-06-05 15:05:19 -0600

    Actualizar dash.js

--- dash.js
+++ dash.js
@@ -525,8 +525,8 @@ window.addEventListener("beforeunload", () => {
 
 
 const COORDENADAS_ESCUELA = {
-  latitud: 20.134403,//20.119646,  // Coordenadas del salon
-  longitud:  -98.766111,//-98.779359,
+  latitud: 20.119646,  // Coordenadas del salon
+  longitud: -98.779359,
   radio: 1000 // Radio en metros permitido
 };
 
