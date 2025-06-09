bf8eca144092586894d9d63e237cf9abb13e0d01
RYOLT <nonoyoltzin@gmail.com>
2025-06-08 20:50:32 -0600

    Version 1.3.2

--- dash.js
+++ dash.js
@@ -525,9 +525,9 @@ window.addEventListener("beforeunload", () => {
 
 
 const COORDENADAS_ESCUELA = {
-  latitud: 20.134322, //20.134403,//20.119646,  // Coordenadas del salon
-  longitud: -98.766224, //-98.766111,//-98.779359,
-  radio: 5000 // Radio en metros permitido
+  latitud: 20.119640,// Coordenadas del salon
+  longitud: -98.779357, 
+  radio: 10000 // Radio en metros permitido
 };
 
 // Función para calcular distancia entre dos puntos geo en metros
